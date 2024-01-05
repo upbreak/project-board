ef929e4b1f5588c3e9208995989e76e6f0f39ab4
jinwoo <upbreak05@kakao.com>
2024-01-05 10:58:51 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가 표현은 추후에 업데이트될 수 있음.

--- src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
+++ src/test/java/com/fastcampus/projectboard/FastCampusProjectBoardApplicationTests.java
@@ -1,4 +1,4 @@
-package com.fastcampus.fastcampusprojectboard;
+package com.fastcampus.projectboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
