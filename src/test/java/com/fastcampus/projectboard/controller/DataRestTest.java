4271d28762da889ff1a16548dc8c85bd347d3bc0
jinwoo <upbreak05@kakao.com>
2024-01-06 16:19:37 +0900

    #1 - data rest 테스트
    꼭 필요하지 않지만 공부겸 작성
    data rest테스트는 인테그레이션으 테스트로 접근할 수 밖에 없다.
    DB접근이 일어나므로 안전을 위해 @Transactional을 적용
    테스트 안에서 롤백 정책으로 적용

---
+++ src/test/java/com/fastcampus/projectboard/controller/DataRestTest.java
@@ -0,0 +1,77 @@
+package com.fastcampus.projectboard.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DisplayName("data rest - api테스트")
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest
+public class DataRestTest {
+
+    private final MockMvc mvc;
+
+    public DataRestTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("api 게시글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestArticles_thenReturnsArticlesResponse() throws Exception {
+
+        mvc.perform(get("/api/articles"))
+                .andExpect(status().isOk()) // ctrl + space - alr + enter -> static생성
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+//                .andDo(print()); // ctrl + space - alr + enter -> static생성
+    }
+
+    @DisplayName("api 게시글 단건 조회")
+    @Test
+    void givenNothing_whenRequestArticle_thenReturnsArticleResponse() throws Exception {
+
+        mvc.perform(get("/api/articles/1"))
+                .andExpect(status().isOk()) // ctrl + space - alr + enter -> static생성
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+//                .andDo(print()); // ctrl + space - alr + enter -> static생성
+    }
+
+    @DisplayName("api 게시글 -> 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestArticleCommentFromArticle_thenReturnsArticleCommentsResponse() throws Exception {
+
+        mvc.perform(get("/api/articles/1/articleComments"))
+                .andExpect(status().isOk()) // ctrl + space - alr + enter -> static생성
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+//                .andDo(print()); // ctrl + space - alr + enter -> static생성
+    }
+
+    @DisplayName("api 댓글 리스트 조회")
+    @Test
+    void givenNothing_whenRequestArticleComments_thenReturnsArticleCommentsResponse() throws Exception {
+
+        mvc.perform(get("/api/articleComments"))
+                .andExpect(status().isOk()) // ctrl + space - alr + enter -> static생성
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+//                .andDo(print()); // ctrl + space - alr + enter -> static생성
+    }
+
+    @DisplayName("api 댓글 단건 조회")
+    @Test
+    void givenNothing_whenRequestArticleComment_thenReturnsArticleCommentResponse() throws Exception {
+
+        mvc.perform(get("/api/articleComments/1"))
+                .andExpect(status().isOk()) // ctrl + space - alr + enter -> static생성
+                .andExpect(content().contentType(MediaType.valueOf("application/hal+json")));
+//                .andDo(print()); // ctrl + space - alr + enter -> static생성
+    }
+}
