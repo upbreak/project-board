faea322550cd97a3c1b16307ee2fb4ae9101ab64
jinwoo <upbreak05@kakao.com>
2024-01-08 18:41:11 +0900

    #16 - 유저계정 도메인 작성

---
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -0,0 +1,57 @@
+package com.fastcampus.projectboard.domain;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "userId", unique = true),
+        @Index(columnList = "email"),
+        @Index(columnList = "createAt"),
+        @Index(columnList = "createBy")
+})
+@Entity
+public class UserAccount extends AuditingFields{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @Column(nullable = false, length = 50) private String userId;
+    @Setter @Column(nullable = false) private String userPassword;
+    @Setter @Column(nullable = false, length = 100) private String email;
+    @Setter @Column(nullable = false, length = 100) private String nickname;
+    @Setter @Column(nullable = false, length = 255) private String memo;
+
+    protected UserAccount() {}
+
+    private UserAccount(String userId, String userPassword, String email, String nickname, String memo) {
+        this.userId = userId;
+        this.userPassword = userPassword;
+        this.email = email;
+        this.nickname = nickname;
+        this.memo = memo;
+    }
+
+    public static UserAccount of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccount(userId, userPassword, email, nickname, memo);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserAccount that)) return false;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
