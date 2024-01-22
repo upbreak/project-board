dc55660b6bb091dab40aab640406cf7de590b150
jinwoo <upbreak05@kakao.com>
2024-01-22 14:43:30 +0900

    #35 - 대댓글 비즈니스 로직 구현
    부모/자식 관계를 만들면서  entity <-> dto간 변환 로직, 댓글 서비스에서 댓글과 대댓글을 저장하고 삭제하는 방법, 댓글과 대댓글을 db 데이터로부터 변환할 때 서로 계층 계층 구조를 만들고 각각 정렬하는 과정을 구현

--- src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
+++ src/main/java/com/fastcampus/projectboard/dto/response/ArticleCommentResponse.java
@@ -3,6 +3,9 @@ package com.fastcampus.projectboard.dto.response;
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
 
 public record ArticleCommentResponse(
         Long id
@@ -11,9 +14,19 @@ public record ArticleCommentResponse(
         , String email
         , String nickname
         , String userId
+        , Long parentCommentId
+        , Set<ArticleCommentResponse> childComments
 ) {
     public static ArticleCommentResponse of(Long id, String content, String userId, LocalDateTime createAt, String email, String nickname) {
-        return new ArticleCommentResponse(id, content, createAt, email, nickname, userId);
+        return ArticleCommentResponse.of(id, content, userId, createAt, email, nickname, null);
+    }
+
+    public static ArticleCommentResponse of(Long id, String content, String userId, LocalDateTime createAt, String email, String nickname, Long parentCommentId) {
+        //화면에 보여줄 때 대댓글을 시간순 오름차순으로 보여줄 것이기 때문에 Comparator를 사용하여 정렬을 하였다.
+        Comparator<ArticleCommentResponse> cildCommentComparator = Comparator
+                .comparing(ArticleCommentResponse::createAt)//오름차순이 디폴트, 내림차순을 할 경우 .reversed()를 붙이면 된다.
+                .thenComparingLong(ArticleCommentResponse::id);
+        return new ArticleCommentResponse(id, content, createAt, email, nickname, userId, parentCommentId, new TreeSet<>(cildCommentComparator));
     }
 
     public static ArticleCommentResponse from(ArticleCommentDto dto){
@@ -22,13 +35,19 @@ public record ArticleCommentResponse(
             nickname = dto.userAccountDto().userId();
         }
 
-        return new ArticleCommentResponse(
+        return ArticleCommentResponse.of(
                 dto.id()
                 , dto.content()
+                , dto.userAccountDto().userId()
                 , dto.createAt()
                 , dto.userAccountDto().email()
                 , nickname
-                , dto.userAccountDto().userId()
+                , dto.parentCommentId()
         );
     }
+
+    public boolean hasParentCommet(){
+        return parentCommentId != null;
+    }
+
 }
--- src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
+++ src/test/java/com/fastcampus/projectboard/service/ArticleCommentServiceTest.java
@@ -16,6 +16,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -85,13 +86,41 @@ class ArticleCommentServiceTest {
         then(articleCommentRepository).should().deleteByIdAndUserAccount_UserId(articleCommentId, userId);
     }
 
+    @DisplayName("부모 댓글 ID와 댓글 정보를 입력하면, 대댓글을 저장한다.")
+    @Test
+    void givenParentCommentIdAndArticleCommentInfo_whenSaving_thenSavesChildComment() {
+        // Given
+        Long parentCommentId = 1L;
+        ArticleComment parent = createArticleComment(parentCommentId, "댓글");
+        ArticleCommentDto child = createArticleCommentDto(parentCommentId, "대댓글");
+        given(articleRepository.getReferenceById(child.articleId())).willReturn(createArticle());
+        given(userAccountRepository.getReferenceById(child.userAccountDto().userId())).willReturn(createUserAccount());
+        given(articleCommentRepository.getReferenceById(child.parentCommentId())).willReturn(parent);
 
+        // When
+        sut.saveArticleComment(child);
 
+        // Then
+        assertThat(child.parentCommentId()).isNotNull();
+        then(articleRepository).should().getReferenceById(child.articleId());
+        then(userAccountRepository).should().getReferenceById(child.userAccountDto().userId());
+        then(articleCommentRepository).should().getReferenceById(child.parentCommentId());
+        then(articleCommentRepository).should(never()).save(any(ArticleComment.class));
+    }
 
     private ArticleCommentDto createArticleCommentDto(String content) {
+        return createArticleCommentDto(null, content);
+    }
+
+    private ArticleCommentDto createArticleCommentDto(Long parentCommentId, String content) {
+        return createArticleCommentDto(1L, parentCommentId, content);
+    }
+
+    private ArticleCommentDto createArticleCommentDto(Long id, Long parentCommentId, String content) {
         return ArticleCommentDto.of(
                 1L,
                 createUserAccountDto(),
+                parentCommentId,
                 content
         );
     }
@@ -106,12 +135,16 @@ class ArticleCommentServiceTest {
         );
     }
 
-    private ArticleComment createArticleComment(String content) {
-        return ArticleComment.of(
+    private ArticleComment createArticleComment(Long id, String content) {
+        ArticleComment articleComment = ArticleComment.of(
                 createArticle(),
                 createUserAccount(),
                 content
         );
+
+        ReflectionTestUtils.setField(articleComment, "id", id);
+
+        return articleComment;
     }
 
     private UserAccount createUserAccount() {
