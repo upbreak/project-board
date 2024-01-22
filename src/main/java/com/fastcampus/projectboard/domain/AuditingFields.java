4f904456825ea6ad7375b271ea30f88831cfb2c2
jinwoo <upbreak05@kakao.com>
2024-01-22 23:06:18 +0900

    AuditingFields 수정
    AuditingFields 클래스는 추상 클래스이고, 각 필드는 상속 받는 자식 엔티티에서 접근 및 수정이 가능해야 한다.
    따라서 접근 제어자를 protected로 수정하였다

--- src/main/java/com/fastcampus/projectboard/domain/AuditingFields.java
+++ src/main/java/com/fastcampus/projectboard/domain/AuditingFields.java
@@ -23,18 +23,18 @@ public abstract class AuditingFields {
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @CreatedDate
     @Column(nullable = false, updatable = false)
-    private LocalDateTime createAt; //생성일지
+    protected LocalDateTime createAt; //생성일지
 
     @CreatedBy
     @Column(nullable = false, length = 100, updatable = false)
-    private String createBy;        //생성자
+    protected String createBy;        //생성자
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     @LastModifiedDate
     @Column(nullable = false)
-    private LocalDateTime modifiedAt;   //수정일시
+    protected LocalDateTime modifiedAt;   //수정일시
 
     @LastModifiedBy
     @Column(nullable = false, length = 100)
-    private String modifiedBy;  //수정자
+    protected String modifiedBy;  //수정자
 }
