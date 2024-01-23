7f4d9ab15fc31ea6e496cb3285dcdfa6216c3368
jinwoo <upbreak05@kakao.com>
2024-01-23 18:23:43 +0900

    #44 - 인증 설정 업데이트: OAuth 인증 설정 추가
    스프링 시큐리티가 기존 DB 인증에다가 OAuth 인증까지
    받아들일 수 있도록 설정 추가
    해당 변경을 테스트에도 반영

--- src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/projectboard/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectboard.config;
 
 import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.dto.security.BoardPrincipal;
+import com.fastcampus.projectboard.dto.security.KakaoOAuth2Response;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
+import com.fastcampus.projectboard.service.UserAccountService;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +15,24 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+            , OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()//정적 리소스를 허용
@@ -32,20 +45,55 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
 //                        .anyRequest().permitAll()//테스트를 위해 인증을 모두 허용상태로 사용
                 )
-                .formLogin(login -> login
-                        .permitAll())
+                .formLogin(withDefaults())
                 .logout(logout -> logout
                         .logoutSuccessUrl("/"))
+                .oauth2Login(oauth -> oauth
+                        .userInfoEndpoint(userinfo -> userinfo
+                                .userService(oAuth2UserService)
+                        )
+                )
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository){
-        return username -> userAccountRepository
-                .findById(username)
-                .map(UserAccountDto::from)
+    public UserDetailsService userDetailsService(UserAccountService userAccountService){
+        return username -> userAccountService.searchUser(username)
                 .map(BoardPrincipal::from)
                 .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다. username + " + username));
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            UserAccountService userAccountService
+            , PasswordEncoder passwordEncoder
+    ){
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId(); //yaml   security.oauth2.client.registration 의 값 'kakao'
+            String providerId = String.valueOf(kakaoResponse.id());
+            String username = registrationId + "_" + providerId;
+            String dummyPassword = passwordEncoder.encode("{bcrypt}dummy" + UUID.randomUUID());
+
+            return userAccountService.searchUser(username)
+                .map(BoardPrincipal::from)
+                .orElseGet(() ->
+                        BoardPrincipal.from(
+                                userAccountService.saveUser(
+                                        username
+                                        , dummyPassword
+                                        , kakaoResponse.email()
+                                        , kakaoResponse.nickname()
+                                        , null
+                                )
+                        )
+                );
+        };
+
     }
 
     @Bean
--- src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
+++ src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
@@ -1,28 +1,36 @@
 package com.fastcampus.projectboard.config;
 
-import com.fastcampus.projectboard.domain.UserAccount;
-import com.fastcampus.projectboard.repository.UserAccountRepository;
+import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.service.UserAccountService;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
 import java.util.Optional;
 
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.anyString;
+import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
 public class TestSecurityConfig {
 
-    @MockBean private UserAccountRepository userAccountRepository;
+    @MockBean private UserAccountService userAccountService;
 
     @BeforeTestMethod
     public void securitySetUp(){
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto(){
+        return UserAccountDto.of(
                 "jinwooTest"
                 , "pw"
                 , "jinwoo@email.com"
                 , "nickname"
                 , "memo"
-        )));
+        );
     }
 }
