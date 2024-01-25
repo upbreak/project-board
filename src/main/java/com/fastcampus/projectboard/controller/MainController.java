ec2e45d954772736f8b8c4ae05c19ebb15a0efee
jinwoo <upbreak05@kakao.com>
2024-01-25 10:35:18 +0900

    프로젝트명 변경
    어드민 서비스 프로젝트랑 통일성을 위해 변경

--- src/main/java/com/fastcampus/projectboard/controller/MainController.java
+++ src/main/java/com/fastcampus/projectboard/controller/MainController.java
@@ -1,8 +1,16 @@
 package com.fastcampus.projectboard.controller;
 
+import com.fastcampus.projectboard.dto.response.ArticleCommentResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.time.LocalDateTime;
+
+/**
+ * 메인 컨트롤러
+ * swagger javadoc 테스트
+ */
 @Controller
 public class MainController {
 
@@ -10,5 +18,24 @@ public class MainController {
     public String root(){
 
         return "forward:/articles";
+    }
+
+    /**
+     * 댓글 정보를 열람한다.
+     *
+     * @param id 댓글ID
+     * @return 댓글 응답
+     */
+    @ResponseBody
+    @GetMapping("/test-rest")
+    public ArticleCommentResponse test(Long id){
+        return ArticleCommentResponse.of(
+                id
+                , "content"
+                , "userId"
+                , LocalDateTime.now()
+                , "email@eamil.com"
+                , "nickname"
+        );
     }
 }
